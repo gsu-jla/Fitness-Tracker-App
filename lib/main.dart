2a3f52e984c5257a66aa2185f245a9f0e07fad0b
gsu-jla <jla2@student.gsu.edu>
2025-03-28 21:11:43 -0400

    homepage added

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,54 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        backgroundColor: Colors.purple,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            buildButton(context, 'Workout Log', Icons.event_note),
+            SizedBox(height: 16),
+            buildButton(context, 'Calorie Tracker', Icons.event_note),
+            SizedBox(height: 16),
+            buildButton(context, 'Progress Tracker', Icons.event_note),
+            SizedBox(height: 16),
+            buildButton(context, 'Workouts', Icons.fitness_center),
+          ],
         ),
+      ),
+    );
+  }
+
+  Widget buildButton(BuildContext context, String title, IconData icon) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(color: Colors.black, width: 3),
+      ),
+      child: ListTile(
+        title: Center(child: Text(title)),
+        trailing: Icon(icon),
+        onTap: () {},
       ),
     );
   }
